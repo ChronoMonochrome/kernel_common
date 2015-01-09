3f87955bdbaf5782bc700ad09ded310fe8afd822
Marcin Kaluza <marcin.kaluza@trioptimum.com>
2015-01-09 16:26:56 +0000

    include_linux
    Conflicts:
    	include/linux/kernel.h

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -743,5 +743,7 @@ struct sysinfo {
 	unsigned int mem_unit;		/* Memory unit size in bytes */
 	char _f[20-2*sizeof(long)-sizeof(int)];	/* Padding: libc5 uses this.. */
 };
+/* To identify board information in panic logs, set this */
+extern char *mach_panic_string;
 
 #endif
