4a0b074eca127d8a50b2d03bc710bb3b77055543
Shilin Victor <chrono.monochrome@gmail.com>
2015-01-22 22:37:48 +0400

    fs_ext4
    Conflicts:
    	fs/ext4/extents.c
    	fs/ext4/mballoc.c
    	fs/ext4/namei.c

--- fs/ext4/mballoc.c
+++ fs/ext4/mballoc.c
@@ -736,6 +736,10 @@ void ext4_mb_generate_buddy(struct super_block *sb,
 	grp->bb_fragments = fragments;
 
 	if (free != grp->bb_free) {
+		/* for more specific debugging, sangwoo2.lee */
+		print_block_data(sb, 0, bitmap, 0, EXT4_BLOCK_SIZE(sb));
+		/* for more specific debugging */
+
 		ext4_grp_locked_error(sb, group, 0, 0,
 				      "%u clusters in bitmap, %u in gd",
 				      free, grp->bb_free);
@@ -1293,7 +1297,10 @@ static void mb_free_blocks(struct inode *inode, struct ext4_buddy *e4b,
 			ext4_fsblk_t blocknr;
 
 			blocknr = ext4_group_first_block_no(sb, e4b->bd_group);
-			blocknr += EXT4_C2B(EXT4_SB(sb), block);
+			blocknr += block;
+			/* for debugging, sangwoo2.lee */
+			print_block_data(sb, blocknr, EXT4_MB_BITMAP(e4b), 0, EXT4_BLOCK_SIZE(sb));
+			/* for debugging */
 			ext4_grp_locked_error(sb, e4b->bd_group,
 					      inode ? inode->i_ino : 0,
 					      blocknr,
