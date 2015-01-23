174a8bf14442b5d168aee41d173ea4ea27313e6b
Shilin Victor <chrono.monochrome@gmail.com>
2015-01-23 21:02:12 +0400

    Revert "include_linux"
    This reverts commit 6dc6bf6c5403238841c6444d52e16688cfc88553.

--- include/linux/device.h
+++ include/linux/device.h
@@ -931,10 +931,6 @@ int _dev_info(const struct device *dev, const char *fmt, ...)
 
 #endif
 
-#if (defined(CONFIG_SAMSUNG_LOG_BUF) || defined(DEBUG))
-#define dev_samsung_dbg(dev, format, arg...)		\
-	dev_printk(KERN_DEBUG , dev , format , ## arg)
-#endif
 /*
  * Stupid hackaround for existing uses of non-printk uses dev_info
  *
