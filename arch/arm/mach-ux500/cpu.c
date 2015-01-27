7c1cc13e3392f442799eac2e410d0d82ef8e88d8
Shilin Victor <chrono.monochrome@gmail.com>
2015-01-27 20:06:03 +0400

    mach-ux500/cpu: fix build error

--- arch/arm/mach-ux500/cpu.c
+++ arch/arm/mach-ux500/cpu.c
@@ -182,9 +182,9 @@ static int __init ux500_l2x0_unlock(void)
 	 * 8 sets of registers, one per possible CPU.
 	 */
 	for (i = 0; i < 8; i++) {
-		writel_relaxed(0x0, l2x0_base + L2X0_LOCKDOWN_WAY_D +
+		writel_relaxed(0x0, l2x0_base + L2X0_LOCKDOWN_WAY_D_BASE +
 			       i * L2X0_LOCKDOWN_STRIDE);
-		writel_relaxed(0x0, l2x0_base + L2X0_LOCKDOWN_WAY_I +
+		writel_relaxed(0x0, l2x0_base + L2X0_LOCKDOWN_WAY_I_BASE +
 			       i * L2X0_LOCKDOWN_STRIDE);
 	}
 	return 0;
