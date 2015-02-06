8c8e4088c836e82a11843e1719aae832b0f03616
Shilin Victor <chrono.monochrome@gmail.com>
2015-02-06 20:22:00 +0400

    include/linux/
    Signed-off-by: Shilin Victor <chrono.monochrome@gmail.com>
    
    Conflicts:
    	include/linux/irq.h
    	include/linux/kernel.h
    	include/linux/of.h
    	include/linux/of_irq.h
    	include/linux/videodev2.h
    
    Conflicts:
    	include/linux/kernel.h

--- include/linux/led-lm3530.h
+++ include/linux/led-lm3530.h
@@ -58,6 +58,12 @@
 #define LM3530_ALS_IMPD_700Ohm		(0x0E)
 #define LM3530_ALS_IMPD_667Ohm		(0x0F)
 
+/*
+ * If lm3530 does not use a gpio for HWEN, set LM3530_NO_HWEN_GPIO
+ * for hw_en_gpio in lm3530_platform data member
+ */
+#define LM3530_NO_HWEN_GPIO			-1
+
 enum lm3530_mode {
 	LM3530_BL_MODE_MANUAL = 0,	/* "man" */
 	LM3530_BL_MODE_ALS,		/* "als" */
@@ -85,6 +91,7 @@ enum lm3530_als_mode {
  * @als1_resistor_sel: internal resistance from ALS1 input to ground
  * @als2_resistor_sel: internal resistance from ALS2 input to ground
  * @brt_val: brightness value (0-255)
+ * @hw_en_gpio: GPIO line for LM3530 HWEN
  */
 struct lm3530_platform_data {
 	enum lm3530_mode mode;
@@ -102,6 +109,8 @@ struct lm3530_platform_data {
 	u8 als2_resistor_sel;
 
 	u8 brt_val;
+
+	int hw_en_gpio;
 };
 
 #endif	/* _LINUX_LED_LM3530_H__ */
