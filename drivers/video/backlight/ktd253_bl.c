c261e2c7b27c593959da081743a709bca2f4487d
Shilin Victor <chrono.monochrome@gmail.com>
2015-02-06 20:16:18 +0400

    drivers/video
    Signed-off-by: Shilin Victor <chrono.monochrome@gmail.com>
    
    Conflicts:
    	drivers/video/backlight/Kconfig
    	drivers/video/backlight/Makefile
    
    Conflicts:
    	drivers/video/backlight/Kconfig

--- drivers/video/backlight/ktd253_bl.c
+++ drivers/video/backlight/ktd253_bl.c
@@ -33,15 +33,8 @@
 #include <video/ktd253x_bl.h>
 
 /* to be removed when driver works */
-/* #define dev_dbg dev_info */
+//#define dev_dbg dev_info
 
-#if defined(CONFIG_MACH_SEC_SKOMER)
-#define DEFAULT_RATIO			13
-#define DEFAULT_BRINGHESS		160
-#else
-#define DEFAULT_RATIO			12
-#define DEFAULT_BRINGHESS		160
-#endif
 
 #define KTD253_BACKLIGHT_OFF		0
 #define KTD253_MIN_CURRENT_RATIO	1	/* 1/32 full current */
@@ -251,8 +244,8 @@ static int ktd253_probe(struct platform_device *pdev)
 	pKtd253Data->currentRatio = KTD253_BACKLIGHT_OFF;
 	pKtd253Data->brightness = KTD253_BACKLIGHT_OFF;
 #else
-	pKtd253Data->currentRatio = DEFAULT_RATIO;
-	pKtd253Data->brightness = DEFAULT_BRINGHESS;
+	pKtd253Data->currentRatio = 12;
+	pKtd253Data->brightness = 160;
 #endif
 	pKtd253Data->pd = pdev->dev.platform_data;
 
@@ -272,8 +265,10 @@ static int ktd253_probe(struct platform_device *pdev)
 		ret = PTR_ERR(ktd253_backlight_device);
 	} else {
 		ktd253_backlight_device->props.power      = FB_BLANK_UNBLANK;
-		ktd253_backlight_device->props.brightness = DEFAULT_BRINGHESS;
+#if 0 // HW request. Down the backlight because of current consumption.
+		ktd253_backlight_device->props.brightness = ktd253_backlight_device->props.max_brightness;
 		ktd253_set_brightness(ktd253_backlight_device);
+#endif
 	}
 
 #ifdef CONFIG_HAS_EARLYSUSPEND
