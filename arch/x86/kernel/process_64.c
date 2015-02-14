9e12ac5f29892a4e6c7fee512abffa1e8bbd1e31
Shilin Victor <chrono.monochrome@gmail.com>
2015-02-14 09:28:04 +0400

    Revert "nohz: Allow rcu extended quiescent state handling seperately from tick stop"
    This reverts commit fc16389bd9de70b705714cea3ab88e293338a7e4.

--- arch/x86/kernel/process_64.c
+++ arch/x86/kernel/process_64.c
@@ -121,7 +121,7 @@ void cpu_idle(void)
 
 	/* endless idle loop with no priority at all */
 	while (1) {
-		tick_nohz_idle_enter_norcu();
+		tick_nohz_idle_enter();
 		while (!need_resched()) {
 
 			rmb();
@@ -147,7 +147,7 @@ void cpu_idle(void)
 			__exit_idle();
 		}
 
-		tick_nohz_idle_exit_norcu();
+		tick_nohz_idle_exit();
 		preempt_enable_no_resched();
 		schedule();
 		preempt_disable();
